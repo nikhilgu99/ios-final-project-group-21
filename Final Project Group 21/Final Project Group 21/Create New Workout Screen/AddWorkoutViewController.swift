002aed17b722fbb1868a054e3f75898b185174ef
MAD2 <mad@MAD2s-MacBook-Pro.local>
2024-12-05 15:10:43 -0500

    workouts screen working

--- Final Project Group 21/Final Project Group 21/Create New Workout Screen/AddWorkoutViewController.swift
+++ Final Project Group 21/Final Project Group 21/Create New Workout Screen/AddWorkoutViewController.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import FirebaseFirestore
+import FirebaseAuth
 
 class AddWorkoutViewController: UIViewController {
     
@@ -31,10 +32,64 @@ class AddWorkoutViewController: UIViewController {
         let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboardOnTap))
         tapRecognizer.cancelsTouchesInView = false
         view.addGestureRecognizer(tapRecognizer)
+        
+        addWorkoutScreen.buttonSaveWorkout.addTarget(self, action: #selector(onSaveWorkoutButtonTapped), for: .touchUpInside)
     }
     
     @objc func hideKeyboardOnTap() {
         view.endEditing(true)
+    }
+    
+    @objc func onSaveWorkoutButtonTapped() {
+        
+        if self.workout == nil {
+            
+            if (addWorkoutScreen.textFieldName.text!.isEmpty) {
+                return
+            }
+            
+            if (addWorkoutScreen.textFieldExerciseInterval.text!.isEmpty) {
+                return
+            }
+
+            self.workout = Workout(name: addWorkoutScreen.textFieldName.text!, exerciseInterval: Int(addWorkoutScreen.textFieldExerciseInterval.text!) ?? 0, exercises:
+                                    [Exercise(name: "Bench Press", sets: 5, reps: 5, rest: 60), Exercise(name: "Front Squat", sets: 5, reps: 3, rest: 45)])
+            //todo add the exercises from the exercise screen into this list rather than these fake ones I put in for testing
+            
+        }
+        
+        //todo update self.workout regardless
+        
+        if let user = Auth.auth().currentUser {
+            let collectionWorkouts = database
+                .collection("users")
+                .document(user.email!)
+                .collection("workouts")
+            do {
+                //print("start add")
+                let ref =  database.collection("users").document(String(user.uid)).collection("workouts").addDocument(data: [
+                    "name": self.workout.name,
+                    "exercise_interval": self.workout.exerciseInterval
+                ])
+                //print("Document added with ID: \(ref.documentID)")
+                
+                //for each exercise, add it
+                for exercise in self.workout.exercises {
+                    
+                    let ref2 =
+                    database.collection("users").document(String(user.uid)).collection("workouts").document(String(ref.documentID)).collection("exercises").addDocument(data: [
+                        "name": exercise.name,
+                        "sets": exercise.sets,
+                        "reps": exercise.reps,
+                        "rest": exercise.rest
+                    ])
+                    
+                }
+                
+            }
+        }
+        
+        navigationController?.popViewController(animated: true)
     }
     
 }
--- Final Project Group 21/Final Project Group 21/Main Screen/ViewController.swift
+++ Final Project Group 21/Final Project Group 21/Main Screen/ViewController.swift
@@ -35,10 +35,88 @@ class ViewController: UIViewController {
         
         mainScreen.buttonProfile.addTarget(self, action: #selector(onButtonProfileTapped), for: .touchUpInside)
         mainScreen.buttonAddWorkout.addTarget(self, action: #selector(onButtonAddWorkoutTapped), for: .touchUpInside)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        /*if let user = Auth.auth().currentUser {
+            self.database.collection("users").document(user.email!).collection("workouts")
+                .addSnapshotListener(includeMetadataChanges: false, listener: {querySnapshot, error in
+                    if let documents = querySnapshot?.documents{
+                        self.workoutsList.removeAll()
+                        for document in documents{
+                            do{
+                                //Add to list
+                                let workoutData = try document.data(with: ServerTimestampBehavior(rawValue: 2)!)//.getDocument(as: Workout.self)
+                                print(workoutData)
+                                
+                                let workout = Workout(name: workoutData["name"] as! String, exerciseInterval: workoutData["exerciseInterval"] as! Int, exercises: [])
+                                
+                                //todo add exercises
+                                
+                                self.workoutsList.append(workout)
+                                print("added workout")
+                                
+                                
+                            }catch{
+                                print(error)
+                            }
+                        }
+                        self.workoutsList.sort(by: {$0.name < $1.name})
+                        self.mainScreen.tableViewWorkouts.reloadData()
+                        print("got here")
+                    }
+                })
+        }*/
+        
+        if let user = Auth.auth().currentUser {
+            let allWorkouts = database.collection("users").document(user.uid).collection("workouts")
+            Task{
+                do {
+                    let querySnapshot = try await allWorkouts.getDocuments()
+                    //print(querySnapshot.documents)
+                    
+                    for document in querySnapshot.documents {
+                        //print("\(document.documentID) => \(document.data())")
+                        let workoutData = try document.data(with: ServerTimestampBehavior(rawValue: 2)!)
+                        //print(workoutData)
+                        
+                        var workout = Workout(name: workoutData["name"] as! String, exerciseInterval: workoutData["exercise_interval"] as! Int, exercises: [])
+                        
+                        //get all exercises, for each one add it to workout
+                        let qs2 = try await allWorkouts.document(document.documentID).collection("exercises").getDocuments()
+                        
+                        for exerciseDoc in qs2.documents {
+                            let exerciseData = try exerciseDoc.data(with: ServerTimestampBehavior(rawValue: 2)!)
+                            
+                            let exercise = Exercise(name: exerciseData["name"] as! String,
+                                                    sets: exerciseData["sets"] as! Int,
+                                                    reps: exerciseData["reps"] as! Int,
+                                                    rest: exerciseData["rest"] as! Int)
+                            workout.exercises.append(exercise)
+                        }
+                        
+                        self.workoutsList.append(workout)
+                        //print("added")
+                        
+                    }
+                    //print(self.workoutsList)
+                    self.workoutsList.sort(by: {$0.name < $1.name})
+                    self.mainScreen.tableViewWorkouts.reloadData()
+
+                } catch {
+                    print("Error getting documents: \(error)")
+                }
+            }
+        }
+        
     }
     
     @objc func onButtonProfileTapped() {
         //todo launch profile screen from here
+        //print(String(self.workoutsList.count) + " workouts found locally")
         let profileVC = ProfileScreenViewController()
         navigationController?.pushViewController(profileVC, animated: true)
     }
@@ -60,6 +138,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Configs.tableViewWorkoutsID, for: indexPath) as! WorkoutsTableViewCell
         cell.labelName.text = workoutsList[indexPath.row].name
+        
         return cell
     }
     
